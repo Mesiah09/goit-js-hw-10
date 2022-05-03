3321a5cf08932c4ccfb693facf7df775968220f6
Mesiah09 <95221557+Mesiah09@users.noreply.github.com>
2022-05-03 22:14:38 +0000

    Deploying to gh-pages from @ Mesiah09/goit-js-hw-10@cff6243991021c0a58b82afbc24ddac7f2643362 🚀

--- src/js/refs.js
+++
@@ -1,5 +0,0 @@
-const countryList = document.querySelector('.country-list');
-const countryInfo = document.querySelector('.country-info');
-const searchBox = document.querySelector('#search-box');
-
-export { countryList, countryInfo, searchBox };
